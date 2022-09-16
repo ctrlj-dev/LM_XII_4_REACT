ff4e05c5c515223d5d9df94a0f4ddbee67852c82
ctrlj <jesuscc1701@gmail.com>
2022-09-16 14:58:57 +0200

    chore: test commit

--- src/components/list.component.tsx
+++ src/components/list.component.tsx
@@ -6,7 +6,7 @@ import { AppLayout } from "../layout/app-layout";
 import { IMembers } from "../models/IMembers";
 import { getMembers } from "../services/getMembers";
 
-//test
+/
 
 export const ListComponents: React.FC = () => {
     const [members, setMembers] = React.useState<IMembers[]>([]);
